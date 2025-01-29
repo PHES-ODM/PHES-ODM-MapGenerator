5d6f7ca21da688b1b4d8db6cc8b08fd14e7ee621
Martin Wellman <mwellman@ohri.ca>
2025-01-29 12:01:08 -0700

    Switched from argparse cli to Typer

--- map_maker/odm_v2/prepare_parts.py
+++ map_maker/odm_v2/prepare_parts.py
@@ -12,9 +12,9 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-import argparse
+import typer
 import pandas as pd
-from typing import Union, List, Any, Optional, Dict
+from typing import Union, List, Any, Optional, Dict, Annotated
 
 from utils.general_utils import save_data_frame, get_logger, expand_multi_rows
 from odm_v2.v2_mapping import V2MappingColumns, V2MappingVariableLocations
@@ -24,76 +24,47 @@ V2_ENUM_NAME_COL = "v2EnumName"
 NA_TAG = "NA"
 AND_TAG = "&"
 
+MAP_COLUMNS = {
+    "version1Table": V2MappingColumns.SOURCE_TABLE,
+    "version1Location": V2MappingColumns.SOURCE_LOCATION,
+    "version1Variable": V2MappingColumns.SOURCE_VARIABLE,
+    "version1Category": V2MappingColumns.SOURCE_CATEGORY,
+}
+
 logger = get_logger(__name__)
 
+app = typer.Typer(
+    pretty_exceptions_show_locals=False,
+    rich_markup_mode="rich",
+)
 
-def select_and_values(df: pd.DataFrame, columns: Union[List[str], str]) -> pd.DataFrame:
-    """In the specified columns of the DataFrame df, select the first item whenever an
-    AND_TAG is found in it, after stripping off spaces and brackets. If no AND_TAG is found
-    then we leave the value unchanged.
+MAIN_HELP = """Prepare the ODM v2 parts file by doing some basic cleaning and
+reorganizing. We will only keep rows corresponding to a mapping
+from a source class to ODM v2. We also expand rows that contain
+data for multiple rows."""
 
-    For example, "(A & B)" and "A&B" and "A" would all result in "A".
+PARTS_FILE_HELP = """Input CSV ODM v2 parts file to prepare for v1 to v2
+                  mapping."""
 
-    Args:
-        df (pd.DataFrame): The DataFrame to select the AND values in. A copy of this is made
-            with the original left unchanged.
-        columns (Union[List[str], str]): The columns to select the AND values in.
-
-    Returns:
-        pd.DataFrame: A copy of df with the first item in any AND values selected.
-    """
-    if isinstance(columns, str):
-        columns = [columns]
-
-    def _select_element(val: Any) -> Any:
-        if not isinstance(val, str) or AND_TAG not in val:
-            return val
-        val = val.strip("() ")
-        val = val.split(AND_TAG)[0]
-        return val.strip("() ")
-
-    df = df.copy()
-    df[columns] = df[columns].map(_select_element)
-    return df
+OUTPUT_FILE_HELP = """CSV file to save the prepared file to."""
 
 
-def add_v1_enumeration_names(df: pd.DataFrame) -> pd.DataFrame:
-    """Add the v1 enumeration names (if any) corresponding to each row of the parts list.
-
-    Args:
-        df (pd.DataFrame): The parts list of ODM v2.
-
-    Returns:
-        pd.DataFrame: The parts list (df) with the v1 enumeration names added. A copy of df is
-            made and the original is left unchanged.
-    """
-
-    def _make_enumeration_name(row: pd.Series) -> str:
-        # If the row has a location of VariableLocations.VARIABLE_CATEGORIES, then it is a member of an enumeration. The
-        # name of the enumeration consists of the concatenation of Columns.SOURCE_TABLE and Columns.SOURCE_VARIABLE (with
-        # the first letter of Columns.SOURCE_VARIABLE capitalized)
-        if (
-            str(row[V2MappingColumns.SOURCE_LOCATION]).lower()
-            == V2MappingVariableLocations.VARIABLE_CATEGORIES.lower()
-        ):
-            table = row[V2MappingColumns.SOURCE_TABLE]
-            variable = row[V2MappingColumns.SOURCE_VARIABLE]
-            return f"{table}{variable[0].upper()}{variable[1:]}"
-        return None
-
-    df = df.copy()
-    df[V2MappingColumns.SOURCE_ENUM_NAME] = df[
-        [
-            V2MappingColumns.SOURCE_LOCATION,
-            V2MappingColumns.SOURCE_TABLE,
-            V2MappingColumns.SOURCE_VARIABLE,
-        ]
-    ].apply(_make_enumeration_name, axis=1)
-    return df
+@app.command(help=MAIN_HELP)
+def main(
+    parts_file: Annotated[str, typer.Option(show_default=False, help=PARTS_FILE_HELP)],
+    output_file: Annotated[
+        str, typer.Option(show_default=False, help=OUTPUT_FILE_HELP)
+    ],
+):
+    logger.info("Making enumeration derivations...")
+    prepare_parts(parts_file, output_file, map_columns=MAP_COLUMNS)
+    logger.info("Finished!")
 
 
 def prepare_parts(
-    parts_file: str, output_file: str, map_columns: Optional[Dict[str, str]] = None
+    parts_file: str,
+    output_file: str,
+    map_columns: Optional[Dict[str, str]] = None,
 ):
     """Prepare the ODM v2 parts file by doing some basic cleaning and reorganizing. We will only
     keep rows corresponding to a mapping from a source class to ODM v2. We also expand rows that
@@ -166,39 +137,78 @@ def prepare_parts(
     save_data_frame(df, output_file, index=False)
 
 
+def select_and_values(df: pd.DataFrame, columns: Union[List[str], str]) -> pd.DataFrame:
+    """In the specified columns of the DataFrame df, select the first item whenever an
+    AND_TAG is found in it, after stripping off spaces and brackets. If no AND_TAG is found
+    then we leave the value unchanged.
+
+    For example, "(A & B)" and "A&B" and "A" would all result in "A".
+
+    Args:
+        df (pd.DataFrame): The DataFrame to select the AND values in. A copy of this is made
+            with the original left unchanged.
+        columns (Union[List[str], str]): The columns to select the AND values in.
+
+    Returns:
+        pd.DataFrame: A copy of df with the first item in any AND values selected.
+    """
+    if isinstance(columns, str):
+        columns = [columns]
+
+    def _select_element(val: Any) -> Any:
+        if not isinstance(val, str) or AND_TAG not in val:
+            return val
+        val = val.strip("() ")
+        val = val.split(AND_TAG)[0]
+        return val.strip("() ")
+
+    df = df.copy()
+    df[columns] = df[columns].map(_select_element)
+    return df
+
+
+def add_v1_enumeration_names(df: pd.DataFrame) -> pd.DataFrame:
+    """Add the v1 enumeration names (if any) corresponding to each row of the parts list.
+
+    Args:
+        df (pd.DataFrame): The parts list of ODM v2.
+
+    Returns:
+        pd.DataFrame: The parts list (df) with the v1 enumeration names added. A copy of df is
+            made and the original is left unchanged.
+    """
+
+    def _make_enumeration_name(row: pd.Series) -> str:
+        # If the row has a location of VariableLocations.VARIABLE_CATEGORIES, then it is a member of an enumeration. The
+        # name of the enumeration consists of the concatenation of Columns.SOURCE_TABLE and Columns.SOURCE_VARIABLE (with
+        # the first letter of Columns.SOURCE_VARIABLE capitalized)
+        if (
+            str(row[V2MappingColumns.SOURCE_LOCATION]).lower()
+            == V2MappingVariableLocations.VARIABLE_CATEGORIES.lower()
+        ):
+            table = row[V2MappingColumns.SOURCE_TABLE]
+            variable = row[V2MappingColumns.SOURCE_VARIABLE]
+            return f"{table}{variable[0].upper()}{variable[1:]}"
+        return None
+
+    df = df.copy()
+    df[V2MappingColumns.SOURCE_ENUM_NAME] = df[
+        [
+            V2MappingColumns.SOURCE_LOCATION,
+            V2MappingColumns.SOURCE_TABLE,
+            V2MappingColumns.SOURCE_VARIABLE,
+        ]
+    ].apply(_make_enumeration_name, axis=1)
+    return df
+
+
 if __name__ == "__main__":
     if "get_ipython" in globals():
+        opts = {
+            "parts_file": "../../gen/odm_v1_to_v2/configs/parts.csv",
+            "output_file": "../../gen/odm_v1_to_v2/configs/parts_prepared.csv",
+        }
 
-        class opts:
-            parts_file = "../../gen/odm_v1_to_v2/configs/parts.csv"
-            output_file = "../../gen/odm_v1_to_v2/configs/parts_prepared.csv"
+        main(**opts)
     else:
-        args = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter
-        )
-        args.add_argument(
-            "--parts_file",
-            type=str,
-            help="Input CSV ODM v2 parts file to prepare for v1 to v2 mapping",
-            required=True,
-        )
-        args.add_argument(
-            "--output_file",
-            type=str,
-            help="CSV file to save the prepared file to",
-            required=True,
-        )
-        opts = args.parse_args()
-
-    logger.info("Making enumeration derivations...")
-
-    map_columns = {
-        "version1Table": V2MappingColumns.SOURCE_TABLE,
-        "version1Location": V2MappingColumns.SOURCE_LOCATION,
-        "version1Variable": V2MappingColumns.SOURCE_VARIABLE,
-        "version1Category": V2MappingColumns.SOURCE_CATEGORY,
-    }
-
-    prepare_parts(opts.parts_file, opts.output_file, map_columns=map_columns)
-
-    logger.info("Finished!")
+        app()
